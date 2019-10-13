27f297f56a513aac4a909c51b7c1ad2730c9e1c6
Student 236916 <236916@student.pwr.edu.pl>
2019-10-13 19:56:12 +0200

    new commit

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,1 +1,33 @@
-#include<iostream>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int ilosc;
+
+	cout << "Podaj wysokosc: ";
+	cin >> ilosc;
+	cout << endl;
+	ilosc -= 1; 
+
+	for (int i = 0; i < ilosc; i++) {            
+		for (int j = ilosc - i; j > 0; j--) {
+			cout << " ";
+		}
+		for (int j = 1; j <= ((i * 2) + 1); j++) {    
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	for (int i = ilosc; i > 0; i--) { 
+		cout << " ";
+	}
+	cout << "#" << endl;
+
+	for (int i = 0; i < ilosc * 2 + 1; i++) {    
+		cout << "-";
+	}
+
+	return 0;
+}
